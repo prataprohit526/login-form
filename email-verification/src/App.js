69cb1092177a6a76a7dacde0c1319fe73875fac8
prataprohit526 <prataprohit526@gmail.com>
2024-06-09 11:06:40 +0530

    add all

--- email-verification/src/App.js
+++ email-verification/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-data">
+        <div className="about">
+          <div className="image-div">
+            <img
+              src="https://www.ez.works/_next/image?url=https%3A%2F%2Fdxw9jueyijhmn.cloudfront.net%2Fez_website%2Ffrontend_img%2FCommonImages%2Fheaderlogo.webp&w=64&q=75"
+              alt="invalid path"
+            />
+            <h1>Work</h1>
+          </div>
+          <div className="content">
+            <h2>Suite of Business Support Services </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              laoreet nisi at elit venenatis fringilla. Cras ut semper quam,
+              sit.
+            </p>
+          </div>
+        </div>
+        <div className="feature">
+          <div>
+            <p>Presentation Design</p>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+          </div>
+          <div>
+            <p>Audio-Visual Production</p>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+          </div>
+          <div>
+            <p>Translation Services</p>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+          </div>
+          <div>
+            <p>Graphic Design</p>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+          </div>
+          <div>
+            <p>Research & Analytics</p>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+          </div>
+          <div>
+            <p>Data Processing</p>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+          </div>
+        </div>
+      </div>
+      <div className="input-feild">
+        <input type="email" placeholder="Enter email" />
+
+        <button>Click me </button>
+      </div>
     </div>
   );
 }
